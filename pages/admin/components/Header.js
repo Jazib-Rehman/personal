4122cc9970ee9bcaf2657f61a97dbc21abd22a60
mkashifse <mk.kashif.se@gmail.com>
2019-10-07 12:21:26 +0500

    header design changes

--- pages/admin/components/Header.js
+++ pages/admin/components/Header.js
@@ -6,8 +6,8 @@ class Header extends React.Component {
 
     render() {
         return (
-            <nav className='flex w-full p-4 border-b bg-white'>
-                <div className="flex-grow">Header</div>
+            <nav className='flex w-full p-4 shadow bg-white'>
+                <div className="flex-grow">Home</div>
                 <div className="flex items-center text-gray-600  ">
                     <Bell className="mr-4"/>
                     <User />
