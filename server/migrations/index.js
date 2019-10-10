c91a17706841d1696b8d43674a2213aeb4d95929
mkashifse <mk.kashif.se@gmail.com>
2019-10-10 11:39:23 +0500

    code clean up

--- server/migrations/index.js
+++ server/migrations/index.js
@@ -1,6 +1,25 @@
+var Sequelize = require('sequelize')
+
 const Products = require('./products.migration')
 const Categories = require('./categories.migration')
 
 
-Products.up()
-Categories.up()
+
+var sequelize = new Sequelize('shawarma', 'root', 'welcome', {
+    host: 'localhost',
+    host: '127.0.0.1',
+    dialect: 'mariadb',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    dialectOptions: {
+        useUTC: false //for reading from database
+    },
+    timezone: 'Etc/GMT0' //for writing to database
+});
+
+
+Products.up(sequelize, Sequelize)
+Categories.up(sequelize, Sequelize)
--- server/migrations/products.migration.js
+++ server/migrations/products.migration.js
@@ -1,24 +1,6 @@
-var Sequelize = require('sequelize')
-
-var sequelize = new Sequelize('shawarma', 'root', 'welcome', {
-    host: 'localhost',
-    host: '127.0.0.1',
-    dialect: 'mariadb',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    },
-    dialectOptions: {
-        useUTC: false //for reading from database
-    },
-    timezone: 'Etc/GMT0' //for writing to database
-});
-
 const Products = {
-    db: {},
-    up() {
-        this.db = sequelize.define('products', {
+    up(sequelize, Sequelize) {
+        sequelize.define('products', {
             name: {
                 type: Sequelize.STRING,
                 field: 'name'
