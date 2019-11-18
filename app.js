8a1f77f90e05668b05a0b4869b5d58b3f53adddc
Jazib-Rehman <jazibrehman30@gmail.com>
2019-11-18 14:37:36 +0500

    api created

---
+++ app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./static/style.css";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            results: {}
+        }
+    }
+
+    render() {
+        <div className="bg-red-500">appp works</div>
+    }
+}
--- server/app.js
+++ server/app.js
@@ -1,47 +1,107 @@
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
 const next = require('next')
-const db = require('./config/database')
 
-const { Meals } = require('./models')
+const app = express();
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = true //process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
 
-const server = express()
-// parse application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({ extended: false }))
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'shawarma'
+});
 
-// parse application/json
-server.use(bodyParser.json())
+connection.connect(err => {
+	if (err) {
+		return err;
+	}
+});
 
-db.authenticate()
-  .then(() => console.log('Database Connected..'))
-  .catch(err => console.log(err))
 
-app.prepare().then(() => {
 
-  server.get('/admin', (req, res) => {
-    Meals.findAll()
-      .then((list) => {
-        app.render(req, res, '/admin', { list: list })
-      })
-  })
+app.use(cors());
 
-  server.post('/admin/meals/add', (req, res) => {
-    const data = req.body;
-    Meals.create(data).then(() => res.send('Product added successfully'))
-    return;
-  })
+app.get('/', (req, res) => {
+	res.send('goto /products')
+});
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+app.get('/products', (req, res) => {
+	connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+		if (err) {
+			return res.send(err);
+		}
+		else {
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
 
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+app.get('/products/add', (req, res) => {
+	const { id, name, description, nutrition, price } = req.query;
+	const INSERT_PPRODUCTS_QUERY = `INSERT INTO products ( id, name, description, nutrition, price ) VALUES('${id}', '${name}', '${description}', '${nutrition}', '${price}')`
+	connection.query(INSERT_PPRODUCTS_QUERY, (err, results) => {
+		if (err) {
+			return res.send(err)
+		} else {
+			return res.send('succesfully added product')
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server Listening on port: 3000')
+});
+
+
+// const express = require('express')
+// var bodyParser = require('body-parser')
+// const next = require('next')
+// const db = require('./config/database')
+
+// const { Meals } = require('./models')
+
+// const port = parseInt(process.env.PORT, 10) || 3000
+// const dev = true //process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+
+// const server = express()
+// // parse application/x-www-form-urlencoded
+// server.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+// server.use(bodyParser.json())
+
+// db.authenticate()
+//   .then(() => console.log('Database Connected..'))
+//   .catch(err => console.log(err))
+
+// app.prepare().then(() => {
+
+//   server.get('/admin', (req, res) => {
+//     Meals.findAll()
+//       .then((list) => {
+//         app.render(req, res, '/admin', { list: list })
+//       })
+//   })
+
+//   server.post('/admin/meals/add', (req, res) => {
+//     const data = req.body;
+//     Meals.create(data).then(() => res.send('Product added successfully'))
+//     return;
+//   })
+
+//   server.all('*', (req, res) => {
+//     return handle(req, res)
+//   })
+
+//   server.listen(port, err => {
+//     if (err) throw err
+//     console.log(`> Ready on http://localhost:${port}`)
+//   })
+// })
