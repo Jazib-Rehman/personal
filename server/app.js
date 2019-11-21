02093f1fc7ad19d7d8255d6e0b171505caa17862
Jazib-Rehman <jazibrehman30@gmail.com>
2019-11-21 16:45:41 +0500

    finale code of the day

--- server/app.js
+++ server/app.js
@@ -1,15 +1,10 @@
 const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql');
-const path = require('path');
 const db = require('./config/database')
 const Categories = require('./models/Categories')
 const Product = require('./models/Product')
 const SubCategories = require('./models/SubCategories')
-
-
-const Sequelize = require('sequelize');
 
 
 db.authenticate()
@@ -22,13 +17,27 @@ app.use(cors());
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-	res.send('goto /products')
+app.post('/add-product', urlencodedParser, (req, res) => {
+	const data = {
+		name: req.body.name,
+		cat_id: req.body.cat_id,
+		subcat_id: req.body.subcat_id,
+		description: req.body.description,
+		nutrition: req.body.nutrition,
+		price: req.body.price,
+		image: req.body.image,
+	}
+	Product.create(data)
+		.then(product => res.send(product))
+		.catch(err => console.log(err));
 });
 
-//Products Routes
-app.use('/products', require('./routes/products'));
-
+app.get('/product', (req, res) =>
+	Product.findAll()
+		.then(products => {
+			res.send(products);
+		})
+		.catch(err => console.log(err)));
 
 app.post('/add-category', urlencodedParser, (req, res) => {
 	const data = {
