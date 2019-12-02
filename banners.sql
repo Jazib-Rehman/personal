ca7b927d56a0ae38b74bf1a151c10c742eb994ca
Jazib-Rehman <jazibrehman30@gmail.com>
2019-12-02 15:54:02 +0500

    databse updated

--- banners.sql
+++ banners.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2019 at 11:36 AM
+-- Generation Time: Dec 02, 2019 at 11:50 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -25,26 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sub_categories`
+-- Table structure for table `banners`
 --
 
-CREATE TABLE `sub_categories` (
+CREATE TABLE `banners` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `cat_id` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `name`, `image`, `createdAt`, `updatedAt`) VALUES
+(1, 'Accelerators', 'uploads/site-headers/2122019145111.jpg', '2019-12-02 09:51:11', '2019-12-02 09:51:11'),
+(2, 'new banner', 'uploads/site-headers/2122019145144.jpg', '2019-12-02 09:51:44', '2019-12-02 09:51:44');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `sub_categories`
+-- Indexes for table `banners`
 --
-ALTER TABLE `sub_categories`
+ALTER TABLE `banners`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -52,10 +59,10 @@ ALTER TABLE `sub_categories`
 --
 
 --
--- AUTO_INCREMENT for table `sub_categories`
+-- AUTO_INCREMENT for table `banners`
 --
-ALTER TABLE `sub_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `banners`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
