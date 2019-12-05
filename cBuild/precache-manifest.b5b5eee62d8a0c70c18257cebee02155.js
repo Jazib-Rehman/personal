13c92ebec352a7820f08e890e1f1d0ed64bd9327
Jazib-Rehman <jazibrehman30@gmail.com>
2019-12-05 14:26:48 +0500

    build ok

---
+++ cBuild/precache-manifest.b5b5eee62d8a0c70c18257cebee02155.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "44908e5397461015cd15271193add5b2",
+    "url": "/static/media/grid.44908e53.png"
+  },
+  {
+    "revision": "b89e9bfa2da98b7f456b72b94426fb05",
+    "url": "/static/media/Blackout-Midnight.b89e9bfa.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c670ec3b21f9e72765a",
+    "url": "/static/js/main.fa189888.chunk.js"
+  },
+  {
+    "revision": "77d441c96986acd1aea8",
+    "url": "/static/js/2.dfe42837.chunk.js"
+  },
+  {
+    "revision": "2c670ec3b21f9e72765a",
+    "url": "/static/css/main.207fb17e.chunk.css"
+  },
+  {
+    "revision": "77d441c96986acd1aea8",
+    "url": "/static/css/2.958a23c6.chunk.css"
+  },
+  {
+    "revision": "01a19348a248464e78f4e176b049f9f2",
+    "url": "/index.html"
+  }
+];
