8f63bfa6973581953efaa015649605fd277a2cce
Jazib-Rehman <jazibrehman30@gmail.com>
2019-12-10 13:04:23 +0500

    design changes

--- src/components/category.js
+++ src/components/category.js
@@ -16,7 +16,7 @@ class Category extends React.Component {
                 <h2 className="font-prim text-prim font-prim relative rounded justify-between title text-3xl">
                     {this.props.category.name}
                 </h2>
-                <div className="flex flex-wrap  mt-8 w-full  m-auto w-full md:w-2/3 ">
+                <div className="flex flex-wrap justify-center mt-8 m-auto md:w-2/3">
                     {
                         this.props.category.products.map((item, j) => (
                             <Meal meal={item} key={j} />
--- src/components/meal.js
+++ src/components/meal.js
@@ -11,15 +11,16 @@ class Meal extends React.Component {
 
     render() {
         return (
-            <div className="w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/6 p-2">
-                <div className="relative cursor-pointer tans-all">
-                    <img className="m-auto w-32 h-32 object-cover rounded-lg zoom-in" src={this.props.meal.image} alt="Shawarma" />
+            <div className="w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 mt-4">
+                <div className="cursor-pointer">
+                    <img className="m-auto w-40 h-40 object-cover rounded-lg zoom-in" src={this.props.meal.image} alt="Shawarma" />
                     <h2 className="text-dark text-md p-2">{this.props.meal.name}</h2>
                     <div className="flex justify-center pb-4">
                         {/* {
-                            this.state.tags.map((tag, i) => (
-                                <div key={i} className="inline-block px-2 rounded-full border bg-gray-600  text-white text-xs mr-1">{tag}</div>
-                            ))
+                            this.state.tags.map((tag, i) => ( */}
+                        <div className="inline-block px-2 rounded-full border bg-gray-600  text-white text-xs mr-1">Spicy</div>
+                        <div className="inline-block px-2 rounded-full border bg-gray-600  text-white text-xs mr-1">Sour</div>
+                        {/* ))
                         } */}
                     </div>
                 </div>
