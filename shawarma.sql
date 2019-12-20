e593c47199ea48363f3568353b327827dd37ca44
Jazib-Rehman <jazibrehman30@gmail.com>
2019-12-20 16:53:37 +0500

    database updates

--- shawarma.sql
+++ shawarma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2019 at 08:49 AM
+-- Generation Time: Dec 20, 2019 at 12:52 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -25,16 +25,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `about_us`
+--
+
+CREATE TABLE `about_us` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `about_us`
+--
+
+INSERT INTO `about_us` (`id`, `title`, `description`, `phone`, `email`, `address`, `created_at`, `updated_at`) VALUES
+(1, 'title new', 'desc', 'phonenumnerr', 'elamlkasj', 'addres location', '2019-12-19 05:53:31', '2019-12-19 10:57:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `banners`
 --
 
 CREATE TABLE `banners` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `name`, `image`, `created_at`, `updated_at`) VALUES
+(5, 'new banner', 'http://localhost:8000/uploads/banners/1576834142.jpg', '2019-12-20 04:29:02', '2019-12-20 04:29:02'),
+(7, 'Ambulances', 'http://localhost:8000/uploads/banners/1576834274.jpg', '2019-12-20 04:31:14', '2019-12-20 04:31:14');
 
 -- --------------------------------------------------------
 
@@ -43,20 +75,26 @@ CREATE TABLE `banners` (
 --
 
 CREATE TABLE `basics` (
-  `id` int(11) NOT NULL,
-  `logo` varchar(255) DEFAULT NULL,
-  `pdf` varchar(255) DEFAULT NULL,
-  `site_header` varchar(255) DEFAULT NULL,
-  `categories` varchar(255) DEFAULT NULL,
-  `channels` varchar(255) DEFAULT NULL,
-  `locator` varchar(255) DEFAULT NULL,
-  `twitter` varchar(255) DEFAULT NULL,
-  `facebook` varchar(255) DEFAULT NULL,
-  `instagram` varchar(255) DEFAULT NULL,
-  `youtube` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `logo` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `site_header` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categories` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `channels` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `locators` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `facebook` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `twitter` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `instagram` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `youtube` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `basics`
+--
+
+INSERT INTO `basics` (`id`, `logo`, `site_header`, `categories`, `channels`, `locators`, `facebook`, `twitter`, `instagram`, `youtube`, `created_at`, `updated_at`) VALUES
+(2, 'http://localhost:8000/uploads/site_headers/1576832980.png', 'kasfjalf', 'asdjaf', 'kjasfj', 'jkasfj', 'jkajklfj', 'jkasjfkl', 'jkajsflj', 'jaksfjl', '2019-12-20 04:06:41', '2019-12-20 04:09:40');
 
 -- --------------------------------------------------------
 
@@ -65,12 +103,19 @@ CREATE TABLE `basics` (
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `image`, `created_at`, `updated_at`) VALUES
+(10, 'kasfjjaflk', 'http://localhost:8000/uploads/categories/1576832067.jpg', '2019-12-20 03:54:28', '2019-12-20 03:54:28');
 
 -- --------------------------------------------------------
 
@@ -79,13 +124,20 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `channels` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `link` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `channels`
+--
+
+INSERT INTO `channels` (`id`, `name`, `link`, `image`, `created_at`, `updated_at`) VALUES
+(6, 'new channel', 'https://www.albertllovera.com/', 'http://localhost:8000/uploads/channels/1576834439.jpg', '2019-12-20 04:33:59', '2019-12-20 04:33:59');
 
 -- --------------------------------------------------------
 
@@ -94,14 +146,21 @@ CREATE TABLE `channels` (
 --
 
 CREATE TABLE `contacts` (
-  `id` int(11) NOT NULL,
-  `f_name` varchar(255) DEFAULT NULL,
-  `l_name` varchar(255) DEFAULT NULL,
-  `email` longtext DEFAULT NULL,
-  `message` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `f_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `l_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `f_name`, `l_name`, `email`, `message`, `created_at`, `updated_at`) VALUES
+(4, 'jazib', 'rehman', 'jazibrehman30@gmail.com', 'my new message', '2019-12-20 04:45:22', '2019-12-20 04:45:22');
 
 -- --------------------------------------------------------
 
@@ -110,12 +169,48 @@ CREATE TABLE `contacts` (
 --
 
 CREATE TABLE `locators` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `map` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `locators`
+--
+
+INSERT INTO `locators` (`id`, `name`, `map`, `image`, `created_at`, `updated_at`) VALUES
+(8, 'Accelerators', 'https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed', 'http://localhost:8000/uploads/locators/1576834735.jpg', '2019-12-20 04:38:55', '2019-12-20 04:38:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2019_12_18_075719_create_channels_table', 1),
+(2, '2019_12_18_093753_create_about_us_table', 1),
+(3, '2019_12_18_093812_create_banners_table', 1),
+(4, '2019_12_18_093825_create_basics_table', 1),
+(5, '2019_12_18_093844_create_categories_table', 1),
+(6, '2019_12_18_093900_create_contacts_table', 1),
+(7, '2019_12_18_093912_create_locators_table', 1),
+(8, '2019_12_18_093927_create_pdfs_table', 1),
+(9, '2019_12_18_093954_create_products_table', 1),
+(10, '2019_12_18_094123_create_tags_table', 1);
 
 -- --------------------------------------------------------
 
@@ -124,12 +219,19 @@ CREATE TABLE `locators` (
 --
 
 CREATE TABLE `pdfs` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pdfs`
+--
+
+INSERT INTO `pdfs` (`id`, `name`, `image`, `created_at`, `updated_at`) VALUES
+(4, 'Accelerators nwe', 'http://localhost:8000/uploads/site_headers/1576833873.jpg', '2019-12-20 04:24:33', '2019-12-20 04:27:49');
 
 -- --------------------------------------------------------
 
@@ -138,36 +240,56 @@ CREATE TABLE `pdfs` (
 --
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `cat_id` varchar(255) DEFAULT NULL,
-  `subcat_id` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `nutrition` varchar(255) DEFAULT NULL,
-  `price` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cat_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nutrition` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `cat_id`, `description`, `nutrition`, `price`, `image`, `created_at`, `updated_at`) VALUES
+(8, 'new name', '3', 'new dessc', 'new nut', 'new price', 'http://localhost:8000/uploads/products/1576828107.jpg', '2019-12-20 02:38:19', '2019-12-20 02:48:27'),
+(9, 'new', '10', 'jklajfkl', 'lkjlakjflk', 'kajfjl', 'http://localhost:8000/uploads/products/1576832302.jpg', '2019-12-20 03:54:38', '2019-12-20 03:58:22');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sub_categories`
+-- Table structure for table `tags`
 --
 
-CREATE TABLE `sub_categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `cat_id` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `tags` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pro_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `pro_id`, `created_at`, `updated_at`) VALUES
+(9, 'new tag', '8', '2019-12-20 02:45:49', '2019-12-20 02:45:49'),
+(10, 'spicy', '8', '2019-12-20 02:48:12', '2019-12-20 02:48:12');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `about_us`
+--
+ALTER TABLE `about_us`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `banners`
@@ -206,6 +328,12 @@ ALTER TABLE `locators`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pdfs`
 --
 ALTER TABLE `pdfs`
@@ -218,9 +346,9 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `sub_categories`
+-- Indexes for table `tags`
 --
-ALTER TABLE `sub_categories`
+ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -228,58 +356,70 @@ ALTER TABLE `sub_categories`
 --
 
 --
+-- AUTO_INCREMENT for table `about_us`
+--
+ALTER TABLE `about_us`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `banners`
 --
 ALTER TABLE `banners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `basics`
 --
 ALTER TABLE `basics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `channels`
 --
 ALTER TABLE `channels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `locators`
 --
 ALTER TABLE `locators`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `pdfs`
 --
 ALTER TABLE `pdfs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `sub_categories`
+-- AUTO_INCREMENT for table `tags`
 --
-ALTER TABLE `sub_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tags`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
