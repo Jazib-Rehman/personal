edda86c2fa6c217dca4e019ed7cc0836b4d0cc12
mkamilkhan <m.kamil.se@gmail.com>
2020-01-08 18:11:33 +0500

    fotter created

--- src/components/category.js
+++ src/components/category.js
@@ -9,7 +9,7 @@ class Category extends React.Component {
   render() {
     return (
       <section className="menu mt-12 m-auto">
-        <h2 className="font-prim text-prim font-prim relative rounded justify-between dash title text-3xl">
+        <h2 className="font-prim text-prim font-prim relative rounded justify-between title text-3xl">
           {this.props.category.name}
         </h2>
         <div className="flex flex-wrap  justify-center mt-8 m-auto md:w-2/3">
