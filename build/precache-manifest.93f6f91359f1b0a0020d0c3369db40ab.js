c2e8e480df887fb0a5a448768f8af3031e21d893
Jazib-Rehman <m.kamil.se@gmail.com>
2020-01-29 17:50:22 +0500

    changes lst

---
+++ build/precache-manifest.93f6f91359f1b0a0020d0c3369db40ab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "519bfb56cd01c76e79bf",
+    "url": "/static/css/main.d0c0ec80.chunk.css"
+  },
+  {
+    "revision": "519bfb56cd01c76e79bf",
+    "url": "/static/js/main.cdbbf441.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0456a0cb9a12f021907",
+    "url": "/static/css/2.859c856e.chunk.css"
+  },
+  {
+    "revision": "c0456a0cb9a12f021907",
+    "url": "/static/js/2.d53667c2.chunk.js"
+  },
+  {
+    "revision": "afd996e0ab17dd0bd08d34bfbd96be41",
+    "url": "/static/media/Capture2.afd996e0.PNG"
+  },
+  {
+    "revision": "b89e9bfa2da98b7f456b72b94426fb05",
+    "url": "/static/media/Blackout-Midnight.b89e9bfa.ttf"
+  },
+  {
+    "revision": "44908e5397461015cd15271193add5b2",
+    "url": "/static/media/grid.44908e53.png"
+  },
+  {
+    "revision": "305912f6381fe6ae77265d8f597665db",
+    "url": "/static/media/new.305912f6.jpg"
+  },
+  {
+    "revision": "e9ad1f641434444df59054b30186d17c",
+    "url": "/static/media/o.e9ad1f64.jpg"
+  },
+  {
+    "revision": "6d6d5aca04585f872cb3edbe3f710f53",
+    "url": "/static/media/as.6d6d5aca.png"
+  },
+  {
+    "revision": "3acca8d4e966ff150da114cce13ea2a8",
+    "url": "/static/media/bg.3acca8d4.png"
+  },
+  {
+    "revision": "2d7615e674db9a081a3c48b10ceb7630",
+    "url": "/index.html"
+  }
+];
