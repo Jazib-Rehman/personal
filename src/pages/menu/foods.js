18549e68c9b1b0fdf087220b500fa1b19d372dff
Jazib-Rehman <m.kamil.se@gmail.com>
2020-01-31 15:12:06 +0500

    changes

--- src/pages/menu/foods.js
+++ src/pages/menu/foods.js
@@ -9,6 +9,7 @@ class Foods extends React.Component {
 		super(props);
 		this.state = {
 			categories: [],
+			basics: [],
 			unsigned: []
 		};
 	}
@@ -19,11 +20,21 @@ class Foods extends React.Component {
 				this.setState({ categories: response ? response : [] });
 			})
 			.catch(err => console.error(err));
+
 		AppService.get("unsign-products/get")
 			.then(response => {
 				this.setState({ unsigned: response ? response : [] });
 			})
 			.catch(err => console.error(err));
+
+		AppService.get("basics/get")
+			.then(response => {
+				this.setState({
+					basics: response ? response : []
+				});
+			})
+			.catch(err => console.error(err));
+
 	}
 
 	handleScroll(id) {
@@ -52,52 +63,56 @@ class Foods extends React.Component {
 	}
 
 	render() {
-		return (
-			<section className="relative text-center py-12 -mb-32 bg-none parallax2">
+		{
+			return this.state.basics.map((item, i) => {
+				return (
+					<section key={i} style={{ backgroundImage: `url(${item.steckers})` }} className="relative text-center py-12 -mb-32 bg-none parallax">
 
 
 
-				<h1 className="flex items-center justify-center font-prim text-dark">
-					<span className="text-lg px-2 text-prim">OUR</span>
-					<span className="text-3xl border-l-2 border-orange-400 px-2 text-prim">
-						{" "}
-						FOOD{" "}
-					</span>
-				</h1>
-				<p className="mt-4 text-gray-500 font-light text-sm">
-					Here you will find our full menu</p>
+						<h1 className="flex items-center justify-center font-prim text-dark">
+							<span className="text-lg px-2 text-prim">OUR</span>
+							<span className="text-3xl border-l-2 border-orange-400 px-2 text-prim">
+								{" "}
+								FOOD{" "}
+							</span>
+						</h1>
+						<p className="mt-4 text-gray-500 font-light text-sm">
+							Here you will find our full menu</p>
 
-				<div className="flex justify-center">
-					<div className="flex flex-wrap w-full md:w-2/3 font-sec justify-center cursor-pointer py-4">
-						{this.state.categories.map((menu, i) => (
-							<div
-								key={i}
-								className="hover:text-white focus:bg-orange-600 text-lg"
-							>
-								<a
-									className="inline-block btn bg-dark-trans rounded-none"
-									onClick={this.handleScroll.bind(this, menu.id)}
-								>
-									{menu.name}
-								</a>
+						<div className="flex justify-center">
+							<div className="flex flex-wrap w-full md:w-2/3 font-sec justify-center cursor-pointer py-4">
+								{this.state.categories.map((menu, i) => (
+									<div
+										key={i}
+										className="hover:text-white focus:bg-orange-600 text-lg"
+									>
+										<a
+											className="inline-block btn bg-dark-trans rounded-none"
+											onClick={this.handleScroll.bind(this, menu.id)}
+										>
+											{menu.name}
+										</a>
+									</div>
+								))}
 							</div>
-						))}
-					</div>
-				</div>
-				<div className="flex flex-wrap mt-8 m-auto w-full md:w-2/3">
-					{this.unasigned.map((item, j) => (
-						<Meal meal={item} key={j} />
-					))}
-				</div>
-				{this.categories.map((category, i) => {
-					return (
-						<div id={category.id} key={i} className="mt-5">
-							<Category category={category} key={category.id} />
 						</div>
-					);
-				})}
-			</section>
-		);
+						<div className="flex flex-wrap mt-8 m-auto w-full md:w-2/3">
+							{this.unasigned.map((item, j) => (
+								<Meal meal={item} key={j} />
+							))}
+						</div>
+						{this.categories.map((category, i) => {
+							return (
+								<div id={category.id} key={i} className="mt-5">
+									<Category category={category} key={category.id} />
+								</div>
+							);
+						})}
+					</section>
+				);
+			})
+		}
 	}
 }
 
